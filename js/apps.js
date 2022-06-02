b4364c8cc0657892ab8fe2a522248948aa971d82
Michael Harris <snufalufakis@gmail.com>
2022-06-02 14:09:37 -0700

    Update to the pokemon api, can now display the name,type, an basic stats, along with the image of the pokemon. will need to set up a proper way to display 2 sets.

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,6 @@
 // simple random number generator to pick a pokemon
 var min =-1;
-var max =152;
+var max =7;
 var diceResults = Math.floor(Math.random() * (max - min)) + min;
 console.log (diceResults);
 // api url
@@ -9,15 +9,30 @@ var api_url = (`https://pokeapi.co/api/v2/pokemon/${diceResults}`);
 async function getRandomPokemon(){
     const response = await fetch (api_url);
     const data = await response.json();
-    const { name } = data;
-    const { stats } = data;
-    var { types,[0]:type} = data;
-    console.log (data);
+    // Name of the pokemon
+    var { name } = data;
+    //Element of the pokemon
+    var {types,[0]:type,name} = data;
+    //Hp of the pokemon
+    var {stats,[0]:base_stat} = data;
+    //Attack of the pokemon
+    var {stats,[1]:base_stat} = data;
+    //Defense of the pokemon
+    var {stats,[2]:base_stat} = data;
+    //Pull the offical artwork of the pokemon
+    imageUrl = {sprites,other,["official-artwork"]:front_default} = data;
+// proof of the pokemon stats and img
     document.getElementById('name').textContent = name;
-    document.getElementById('types').textContent = types;
-    document.getElementById('stats').textContent = stats;
+    document.getElementById('types').textContent = types[0].type.name;
+    document.getElementById('hp').textContent = stats[0].base_stat;
+    document.getElementById('attack').textContent = stats[1].base_stat;
+    document.getElementById('defense').textContent = stats[2].base_stat;
+// json image source
+    img.src = sprites.other["official-artwork"].front_default;
+
+    console.log (data);
+    console.log (img);
     
-}
+};
 getRandomPokemon();
 
-// still need to pull stats(hp, damage, defence), type(pokemon element) and, stock png images
