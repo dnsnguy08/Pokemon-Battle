d9dab52128450a414ddd46e89f333434052e8b8d
Michael Harris <snufalufakis@gmail.com>
2022-06-03 21:31:21 -0700

    Changes to script.js, simplified the functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,84 @@
-// Variables for player hands
-const player1 = 1;
-const player2 = 2;
-const player1Hand = [];
-const player2Hand =[];
-const max = 151; // Game using only 151 original Pokemon
-
 // Function for randomzing player1 and player 2 decks to choose Pokemon from
-function generateHands() {
+function generateHand() {
+  const max = 151; // Game using only 151 original Pokemon
+  let playerHand = [];
   for (let i = 0; i < 6; i++) {
-    var pickPokemon1 = Math.floor(Math.random() * max);
-    player1Hand[i] = pickPokemon1;
-    var pickPokemon2 = Math.floor(Math.random() * max);
-    player2Hand[i] = pickPokemon2;
+    var pokemonId = Math.floor(Math.random() * max);
+    playerHand[i] = pokemonId;
   }
+  return playerHand;
 }
 
-generateHands();
-
-// Dice Roll API Key
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'roll-dice1.p.rapidapi.com',
-    'X-RapidAPI-Key': 'a3a4e80f16msh76859c9ae8fbee0p1baf97jsn009677e9d3e0'
-  }
-};
-
 // Roll Dice to pick a random pokemon from player hands and display on page
-function getRandomPokemon(player) {
-  fetch('https://roll-dice1.p.rapidapi.com/rollDice', options) // Player 1 Dice roll
-  
+const getRandomNumber = async () => {
+  // Dice Roll API Key
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'roll-dice1.p.rapidapi.com',
+      'X-RapidAPI-Key': 'a3a4e80f16msh76859c9ae8fbee0p1baf97jsn009677e9d3e0'
+    }
+  };
+  const result = fetch('https://roll-dice1.p.rapidapi.com/rollDice', options) // Player 1 Dice roll
   .then(function(response){
     return response.json();
   })
   .then(function(data){
-    let result = data.data.Dice
-    if (player === 1) {
-      var api_url = `https://pokeapi.co/api/v2/pokemon/${player1Hand[result-1]}`; // Create API call based on dice roll result
-    } else {
-      var api_url = `https://pokeapi.co/api/v2/pokemon/${player2Hand[result-1]}`; // Create API call based on dice roll result
-    }
-    fetch(api_url, { // Fetch pokemon data based on dice roll
-    })  
-      .then(function(response){
-      return response.json();
-    })
-      .then(function(data){ // apply pokemon stats an image to the page
-        let pokeName = data.name;
-        let pokeType = data.types[0].type.name;
-        let hp = data.stats[0].base_stat;
-        let attack = data.stats[1].base_stat;
-        let defense = data.stats[2].base_stat;
-        let icon = data.sprites.other["official-artwork"].front_default;
-        document.getElementById(`name${player}`).textContent = pokeName;
-        document.getElementById(`types${player}`).textContent = pokeType;
-        document.getElementById(`hp${player}`).textContent = hp;
-        document.getElementById(`attack${player}`).textContent = attack;
-        document.getElementById(`defense${player}`).textContent = defense;
-        document.getElementById(`img${player}`).src = icon;
-      })
-    })
-  }
+    return data.data.Dice;
+  })
+  return result;
+}
 
-getRandomPokemon(player1);
-getRandomPokemon(player2);
+const getRandomPokemon = async (playerHand) => {
+  const randomNumber = await getRandomNumber();
+  const api_url = `https://pokeapi.co/api/v2/pokemon/${playerHand[randomNumber]}`; // Create API call based on dice roll result
+  const result = fetch(api_url, { // Fetch pokemon data based on dice roll
+  })  
+    .then(function(response) {
+      return response.json();
+  })
+    .then(function(data){ // apply pokemon stats an image to the page
+      return data;
+    })
+  return result
+} 
+
+const setPlayerPokemon = (player, pokemon) => {
+  let pokeName = pokemon.name;
+  let pokeType = pokemon.types[0].type.name;
+  let hp = pokemon.stats[0].base_stat;
+  let attack = pokemon.stats[1].base_stat;
+  let defense = pokemon.stats[2].base_stat;
+  let icon = pokemon.sprites.other["official-artwork"].front_default;
+  document.getElementById(`name${player}`).textContent = pokeName;
+  document.getElementById(`types${player}`).textContent = pokeType;
+  document.getElementById(`hp${player}`).textContent = hp;
+  document.getElementById(`attack${player}`).textContent = attack;
+  document.getElementById(`defense${player}`).textContent = defense;
+  document.getElementById(`img${player}`).src = icon;
+}
+
+const main = async () => {
+  // Variables for player hands
+  const players = {
+    one: {
+      id: 1,
+      hand: generateHand(),
+      pokemon: undefined,
+    },
+    two: {
+      id: 2,
+      hand: generateHand(),
+      pokemon: undefined,
+    },
+  }
+  const battleState = {
+    
+  }
+  players.one.pokemon = await getRandomPokemon(players.one.hand);
+  players.two.pokemon =  await getRandomPokemon(players.two.hand);
+  console.log(players.one.pokemon, players.two.pokemon);
+  setPlayerPokemon(players.one.id, players.one.pokemon);
+  setPlayerPokemon(players.two.id, players.two.pokemon);
+};
+main();
