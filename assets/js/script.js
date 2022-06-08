02d205aead4a6c7d7c7857c824b88948c6e701ed
Michael Harris <snufalufakis@gmail.com>
2022-06-08 14:11:17 -0700

    Small Changes to name logic, some can't be changed due to formulas. added a hide button for generating deck after click. delay is wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
 // Variables for player hands
-const player1 = 1;
-const player2 = 2;
-const player1Hand = [];
-const player2Hand = [];
+const playerOne = 1;
+const playerTwo = 2;
+const playerOneHand = [];
+const playerTwoHand = [];
 const max = 151; // Game using only 151 original Pokemon
 var activeCardTwo = document.getElementById("player2Card");
-var activeCardOne = document.getElementById("player1Card");
+var activeCardOne = document.getElementById("playerOneCard");
 var diceResult;
 var pickPokemon; // variable for random pokemon when dice index is undefined in player decks
 
-// Function for randomzing player1 and player 2 decks to choose Pokemon from
+// Function for randomzing playerOne and player 2 decks to choose Pokemon from
 function generateHands() {
   for (let i = 0; i < 6; i++) {
     var pickPokemon1 = Math.floor(Math.random() * max);
-    player1Hand[i] = pickPokemon1;
+    playerOneHand[i] = pickPokemon1;
     var pickPokemon2 = Math.floor(Math.random() * max);
-    player2Hand[i] = pickPokemon2;
+    playerTwoHand[i] = pickPokemon2;
   }
-  console.log(player1Hand);
-  console.log(player2Hand);
+  console.log(playerOneHand);
+  console.log(playerTwoHand);
   // Display pokemon images in player deck cards
-  for (let i = 0; i < player1Hand.length; i++) {
-    var api_url = `https://pokeapi.co/api/v2/pokemon/${player1Hand[i]}`;
+  for (let i = 0; i < playerOneHand.length; i++) {
+    var api_url = `https://pokeapi.co/api/v2/pokemon/${playerOneHand[i]}`;
     fetch(api_url, { // Fetch pokemon data
     })  
       .then(function(response){
@@ -32,8 +32,8 @@ function generateHands() {
         document.querySelector(`#pic1${i}`).src = icon;
       })
     }
-  for (let i = 0; i < player2Hand.length; i++) {
-    var api_url = `https://pokeapi.co/api/v2/pokemon/${player2Hand[i]}`;
+  for (let i = 0; i < playerTwoHand.length; i++) {
+    var api_url = `https://pokeapi.co/api/v2/pokemon/${playerTwoHand[i]}`;
     fetch(api_url, { // Fetch pokemon data
     })  
       .then(function(response){
@@ -67,22 +67,22 @@ function getRandomPokemon(player) {
     console.log(`dice roll: ${diceResult}`);
     console.log(`hand index: ${diceResult -1}`);
     
-    if (player === 1 && player1Hand[diceResult-1] === undefined) {
-      pickPokemon = Math.floor(Math.random() * player1Hand.length); // randomize pokemon summon if dice roll index does not apply
-      let getPokemon = player1Hand[pickPokemon];
+    if (player === 1 && playerOneHand[diceResult-1] === undefined) {
+      pickPokemon = Math.floor(Math.random() * playerOneHand.length); // randomize pokemon summon if dice roll index does not apply
+      let getPokemon = playerOneHand[pickPokemon];
       console.log(getPokemon);
       var api_url = `https://pokeapi.co/api/v2/pokemon/${getPokemon}`; // Create API call based on remaining pokemon in player hand
     } else if (player === 1) {
-      var api_url = `https://pokeapi.co/api/v2/pokemon/${player1Hand[diceResult-1]}`; // Create API call based on dice roll result
+      var api_url = `https://pokeapi.co/api/v2/pokemon/${playerOneHand[diceResult-1]}`; // Create API call based on dice roll result
     }
 
-    if (player === 2 && player2Hand[diceResult-1] === undefined) {
-      pickPokemon = Math.floor(Math.random() * player2Hand.length)
-      let getPokemon = player2Hand[pickPokemon];
+    if (player === 2 && playerTwoHand[diceResult-1] === undefined) {
+      pickPokemon = Math.floor(Math.random() * playerTwoHand.length)
+      let getPokemon = playerTwoHand[pickPokemon];
       console.log(getPokemon);
       var api_url = `https://pokeapi.co/api/v2/pokemon/${getPokemon}`; // Create API call based on remaining pokemon in player hand
     } else if (player === 2) {
-      var api_url = `https://pokeapi.co/api/v2/pokemon/${player2Hand[diceResult-1]}`; // Create API call based on dice roll result
+      var api_url = `https://pokeapi.co/api/v2/pokemon/${playerTwoHand[diceResult-1]}`; // Create API call based on dice roll result
     }
 
     fetch(api_url, { // Fetch pokemon data based on dice roll
@@ -93,12 +93,12 @@ function getRandomPokemon(player) {
       .then(function(data){ // apply pokemon stats an image to the page
         let pokeName = data.name;
         let pokeType = data.types[0].type.name;
-        let hp = data.stats[0].base_stat;
+        let hitPoints = data.stats[0].base_stat;
         let attack = data.stats[1].base_stat;
         let icon = data.sprites.other["official-artwork"].front_default;
         document.getElementById(`name${player}`).textContent = pokeName;
         document.getElementById(`types${player}`).textContent = pokeType;
-        document.getElementById(`hp${player}`).textContent = hp;
+        document.getElementById(`hitPoints${player}`).textContent = hitPoints;
         document.getElementById(`attack${player}`).textContent = attack; 
         document.getElementById(`img${player}`).src = icon;
       })
@@ -120,45 +120,46 @@ generateDeckEl.addEventListener("click",function(){
     generateHands();
     //hide the deck button;
     console.log(generateDeckEl);
+    generateDeckEl.remove();
   }
 );
 
 // Player 1 roll dice and summon pokemon from deck
-var selectPokemon1 = document.querySelector("#rollDice1");
-selectPokemon1.addEventListener("click",function(){
+var selectPokemonOne = document.querySelector("#rollDiceOne");
+selectPokemonOne.addEventListener("click",function(){
   activeCardOne.style.backgroundColor = "yellow";
-  getRandomPokemon(player1);
-  console.log(selectPokemon1);
+  getRandomPokemon(playerOne);
+  console.log(selectPokemonOne);
   }
 );
 
 // Player 2 roll dice and summon pokemon from deck
-var selectPokemon2 = document.querySelector("#rollDice2");
-selectPokemon2.addEventListener("click",function(){
+var selectPokemonTwo = document.querySelector("#rollDiceTwo");
+selectPokemonTwo.addEventListener("click",function(){
   activeCardTwo.style.backgroundColor = "yellow";
-  getRandomPokemon(player2);
-  console.log(selectPokemon1);
+  getRandomPokemon(playerTwo);
+  console.log(selectPokemonTwo);
   }
 );
 
 //Function to Start the Pokemon Battle
 var startBattle = document.querySelector("#startBattle");
 startBattle.addEventListener("click",function(){
-var hp1 = parseInt(document.getElementById("hp1").textContent);
-var hp2 = parseInt(document.getElementById("hp2").textContent);
+var hitPoints1 = parseInt(document.getElementById("hitPoints1").textContent);
+var hitPoints2 = parseInt(document.getElementById("hitPoints2").textContent);
 var attack1 = parseInt(document.getElementById("attack1").textContent);
 var attack2 = parseInt(document.getElementById("attack2").textContent);
   
-  while (hp1 > 0 || hp2 > 0)
+  while (hitPoints1 > 0 || hitPoints2 > 0)
   {
     
-    hp2 -= attack1;
-    var hp2Text = document.getElementById("hp2");
-    hp2Text.textContent = hp2.toString();
+    hitPoints2 -= attack1;
+    var hitPoints2Text = document.getElementById("hitPoints2");
+    hitPoints2Text.textContent = hitPoints2.toString();
     $('#player2Card').shake()
     // delay
-    if (hp2 <= 0){
-      hp2Text.textContent = '0';
+    if (hitPoints2 <= 0){
+      hitPoints2Text.textContent = '0';
       //code for pokemon faint
       activeCardTwo.style.backgroundColor = "red";
       checkPlayerCards(player2Hand); // check player deck and remove fainted pokemon
@@ -166,22 +167,22 @@ var attack2 = parseInt(document.getElementById("attack2").textContent);
       break;
     }
 
-    hp1 -= attack2;
-    var hp1Text = document.getElementById("hp1");
-    hp1Text.textContent = hp1.toString();
-    $('#player1Card').shake()
+    hitPoints1 -= attack2;
+    var hitPoints1Text = document.getElementById("hitPoints1");
+    hitPoints1Text.textContent = hitPoints1.toString();
+    $('#playerOneCard').shake()
     //delay    
-    if (hp1 <= 0){
-      hp1Text.textContent = '0';
+    if (hitPoints1 <= 0){
+      hitPoints1Text.textContent = '0';
       activeCardOne.style.backgroundColor = "red";
-      checkPlayerCards(player1Hand); // check player deck and remove fainted pokemon
-      console.log(player1Hand);
+      checkPlayerCards(playerOneHand); // check player deck and remove fainted pokemon
+      console.log(playerOneHand);
       break;
     }
   }
 });
 // To do list
-//delay
-//hide the button get your decks
+// delay(wip)
+// ✔hide the button get your decks
 //Adding logic for attack priority
-//Clean up variable names
+// ✔ Clean up variable names (some can't be changed due to logic.)
