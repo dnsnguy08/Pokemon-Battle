303c7c62153f5a3e523d5c24144723281f007ec8
ashachakre0906 <chourpagar.asha@gmail.com>
2022-06-07 21:09:58 -0700

    Added variables to start battle function ,updated while loop,added background color when pokemon fainted,splice to remove the fainted pokemon,if condition when the health goes to 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ const player2 = 2;
 const player1Hand = [];
 const player2Hand =[];
 const max = 151; // Game using only 151 original Pokemon
+var bgcolorPlayerCard2 = document.getElementById("player2Card");
+var bgcolorPlayerCard1 = document.getElementById("player1Card");
+var diceResult;
 
 // Function for randomzing player1 and player 2 decks to choose Pokemon from
 function generateHands() {
@@ -60,6 +63,7 @@ function getRandomPokemon(player) {
   })
   .then(function(data){
     let result = data.data.Dice
+    diceResult = data.data.Dice
     console.log(result);
     if (player === 1) {
       var api_url = `https://pokeapi.co/api/v2/pokemon/${player1Hand[result-1]}`; // Create API call based on dice roll result
@@ -91,7 +95,7 @@ window.onload=function(){
 var generateDeckEl = document.querySelector("#generateDeck");
 generateDeckEl.addEventListener("click",function(){
     generateHands();
-    // displayDecks();
+    //hide the deck button;
     console.log(generateDeckEl);
 }
 );
@@ -99,6 +103,7 @@ generateDeckEl.addEventListener("click",function(){
 // Player 1 roll dice and summon pokemon from deck
 var selectPokemon1 = document.querySelector("#roll-dice-1");
 selectPokemon1.addEventListener("click",function(){
+  bgcolorPlayerCard1.style.backgroundColor = "yellow";
   getRandomPokemon(player1);
   console.log(selectPokemon1);
 }
@@ -107,31 +112,57 @@ selectPokemon1.addEventListener("click",function(){
 // Player 2 roll dice and summon pokemon from deck
 var selectPokemon2 = document.querySelector("#roll-dice-2");
 selectPokemon2.addEventListener("click",function(){
+  bgcolorPlayerCard2.style.backgroundColor = "yellow";
   getRandomPokemon(player2);
   console.log(selectPokemon1);
 }
 );
 };
-// function to start the battle
-// poke1Attack = attack
-// poke2Health = hp + defense
-// poke2Attack = attack
-// start battle >>add event listener on click start button
-// var startButton = document.getElementById("start-battle");
-// var hp1 = document.getElementById('hp1').textContent;
-// var startButton = document.getElementById("start-battle");
-// startButton.addEventListener("click",function(){
-//   console.log(hp1);
-// });
 
-// var hp1 = document.getElementById("hp1").textContent
+//Function to Start the Pokemon Battle
+  var startBattle = document.querySelector("#startBattle");
+  startBattle.addEventListener("click",function(){
+  var hp1 = parseInt(document.getElementById("hp1").textContent);
+  var hp2 = parseInt(document.getElementById("hp2").textContent);
+  var attack1 = parseInt(document.getElementById("attack1").textContent);
+  var attack2 = parseInt(document.getElementById("attack2").textContent);
+    
+    while (hp1 > 0 || hp2 > 0)
+    {
+     
+      hp2 -= attack1;
+      var hp2Text = document.getElementById("hp2");
+      hp2Text.textContent = hp2.toString();
+      // delay
+      if (hp2 <= 0){
+        hp2Text.textContent = '0';
+        //code for pokemon faint
+        bgcolorPlayerCard2.style.backgroundColor = "red";
+        player2Hand.splice((diceResult - 1),1);
+        console.log(player2Hand);
+        break;
 
-// var startButton = document.querySelector("#startBattle");
-// startButton.addEventListener("click",function(){
-//     while (parseInt(document.getElementById("hp1").textContent) > 0 || parseInt(document.getElementById("hp2").textContent) > 0)
-//     {
-//       console.log(document.getElementById("hp1").textContent);
-//       break
-//     }
-//   }
-// );
+      }
+      bgcolorPlayerCard1.style.backgroundColor = "yellow";
+      hp1 -= attack2;
+      var hp1Text = document.getElementById("hp1");
+      hp1Text.textContent = hp1.toString();
+      //delay    
+      if (hp1 <= 0){
+        hp1Text.textContent = '0';
+        bgcolorPlayerCard1.style.backgroundColor = "red";
+        player1Hand.splice((diceResult - 1),1);
+        console.log(player1Hand);
+        break;
+      }
+      console.log(hp2Text);
+      console.log(hp1Text);
+
+    } 
+  
+});
+// To do list
+//delay
+//hide the button get your decks
+//Adding logic for attack priority
+//Clean up variable names
